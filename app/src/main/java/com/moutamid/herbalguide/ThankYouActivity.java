494948e7d823decb9ebcb6f1ee1deffde10b6c73
suleman <sunnybajwa904@gmail.com>
2022-10-27 09:48:12 +0500

    commit

--- app/src/main/java/com/moutamid/herbalguide/ThankYouActivity.java
+++ app/src/main/java/com/moutamid/herbalguide/ThankYouActivity.java
@@ -2,13 +2,23 @@ package com.moutamid.herbalguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ThankYouActivity extends AppCompatActivity {
+
+    Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_thank_you);
+        back = findViewById(R.id.back);
+
+        back.setOnClickListener(v -> {
+            startActivity(new Intent(this, ItemsActivity.class));
+            finish();
+        });
     }
 }
