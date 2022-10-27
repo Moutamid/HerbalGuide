052bc3e66c158abe6584959df69eebf5d2a1d9b9
suleman <sunnybajwa904@gmail.com>
2022-10-27 23:44:47 +0500

    commit

--- app/src/main/java/com/moutamid/herbalguide/SurveyActivity.java
+++ app/src/main/java/com/moutamid/herbalguide/SurveyActivity.java
@@ -2,13 +2,21 @@ package com.moutamid.herbalguide;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.moutamid.herbalguide.model.SurveyModel;
 
 public class SurveyActivity extends AppCompatActivity {
 
     Button finishEvaluation;
+    SurveyModel surveyModel;
+    EditText ques1, ques2, ques3, ques4, ques5, ques6, ques7, ques8, ques9, ques10, ques11, ques12;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +24,48 @@ public class SurveyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_survey);
 
         finishEvaluation = findViewById(R.id.finishEvaluation);
+        ques1 = findViewById(R.id.question_1);
+        ques2 = findViewById(R.id.question_2);
+        ques3 = findViewById(R.id.question_3);
+        ques4 = findViewById(R.id.question_4);
+        ques5 = findViewById(R.id.question_5);
+        ques6 = findViewById(R.id.question_6);
+        ques7 = findViewById(R.id.question_7);
+        ques8 = findViewById(R.id.question_8);
+        ques9 = findViewById(R.id.question_9);
+        ques10 = findViewById(R.id.question_10);
+        ques11 = findViewById(R.id.question_11);
+        ques12 = findViewById(R.id.question_12);
+
+        progressDialog = new ProgressDialog(SurveyActivity.this);
+        progressDialog.setMessage("يرجى الانتظار حتى نتلقى تقييمك");
 
         finishEvaluation.setOnClickListener(v -> {
-            startActivity(new Intent(this, ThankYouActivity.class));
-            finish();
+            progressDialog.show();
+            surveyModel = new SurveyModel(
+                    ques1.getText().toString(),
+                    ques2.getText().toString(),
+                    ques3.getText().toString(),
+                    ques4.getText().toString(),
+                    ques5.getText().toString(),
+                    ques6.getText().toString(),
+                    ques7.getText().toString(),
+                    ques8.getText().toString(),
+                    ques9.getText().toString(),
+                    ques10.getText().toString(),
+                    ques11.getText().toString(),
+                    ques12.getText().toString()
+            );
+            Constants.databaseReference().child("surveys").child(Constants.auth().getCurrentUser().getUid())
+                            .setValue(surveyModel)
+                    .addOnSuccessListener(unused -> {
+                        progressDialog.dismiss();
+                        startActivity(new Intent(this, ThankYouActivity.class));
+                        finish();
+                    }).addOnFailureListener(e -> {
+                        progressDialog.dismiss();
+                        Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    });
         });
 
     }
